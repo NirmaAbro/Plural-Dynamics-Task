ff897d14789b6720369fe189b55f20c7c2512082
Nirma Abro <NirmaAbro143@gmail.com>
2024-08-17 02:41:57 -0600

    section three , four , five added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import SectionTwo from "./components/SectionTwo";
 import SectionThree from "./components/SectionThree";
+import img1 from "./images/undraw_happy_news_hxmt.svg";
+import img2 from "./images/undraw_social_influencer_sgsv.svg";
+import img3 from "./images/undraw_mention_6k5d.svg"
 
 function App() {
   return (
@@ -9,7 +12,48 @@ function App() {
       <Navbar />
       <Hero />
       <SectionTwo />
-      <SectionThree />
+      <SectionThree
+        imgSrc={img1}
+        title="Light, Fast & Powerful"
+        description={
+          <>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies <br />
+            nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis
+            enim.
+          </>
+        }
+      />
+      <SectionThree
+        imgSrc={img2}
+        title="Another Title"
+        description={
+          <>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies <br />
+            nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis
+            enim.
+          </>
+        }
+      />
+       <SectionThree
+        imgSrc={img3}
+        title="Another Title"
+        description={
+          <>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies <br />
+            nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis
+            enim.
+          </>
+        }
+      />
     </div>
   );
 }
--- src/components/SectionThree.jsx
+++ src/components/SectionThree.jsx
@@ -1,56 +1,75 @@
-// import React from "react";
-// import img from "../images/undraw_happy_news_hxmt.svg";
+// // import React from "react";
+// // import img from "../images/undraw_happy_news_hxmt.svg";
 
-// function SectionThree() {
+// // function SectionThree() {
+// //   return (
+// //     <div className="block md:flex items-center md:mt-28 mt-36">
+// //       {/* Image Container */}
+// //       <div className="md:w-1/2 justify-center items-center m-3">
+// //         <img src={img} alt="" className="w-full h-full m-auto md:w-2/4" />
+// //       </div>
+// //       {/* Text Content */}
+// //       <div className="md:w-1/2 justify-center items-center leading-2 mt-12 md:mt-0">
+// //         <h1 className="text-3xl font-medium leading-tight text-secondary ml-4">
+// //           Light, Fast & Powerful
+// //         </h1>
+// //         <p className="mt-4 text-lg text-[#6F7CB2] ml-4">
+// //           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+// //           commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+// //           et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+// //           felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+// //           consequat massa quis enim.
+// //         </p>
+// //       </div>
+// //     </div>
+// //   );
+// // }
+
+// // export default SectionThree;
+
+// import React from "react";
+
+// function Section({ imgSrc, title, description }) {
 //   return (
-//     <div className="block md:flex items-center md:mt-28 mt-48 ">
+//     <div className="block md:flex items-center md:mt-28 mt-48 space-x-8">
 //       {/* for img  */}
-//       <div className="justify-center items-center m-3">
-//         <img src={img} alt="" className="w-full h-full m-auto " />
+//       <div className=" mt-7 justify-center items-center mx-14 ml-9 md:2/4">
+//         <img src={imgSrc} alt="" className="w-full h-full m-auto justify-center items-center text-center " />
 //       </div>
 //       {/* for text  */}
-//       <div className="justify-center items-center leading-2 ">
+//       <div className="justify-center items-center leading-2 md:w-1/2 ml-4 mt-9 text-justify mx-3">
 //         <h1 className="text-3xl font-medium leading-tight text-secondary ml-4">
-//           Light, Fast & Powerful
+//           {title}
 //         </h1>
-//         <p className="mt-4 text-lg text-[#6F7CB2] ml-4 ">
-//           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-//           commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
-//           et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
-//           felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
-//           consequat massa quis enim.
-//         </p>
+//         <p className="mt-4 text-lg text-[#6F7CB2] ml-4">{description}</p>
 //       </div>
 //     </div>
 //   );
 // }
 
-// export default SectionThree;
-import React from "react";
-import img from "../images/undraw_happy_news_hxmt.svg";
+// export default Section;
 
-function SectionThree() {
+import React from "react";
+
+function Section({ imgSrc, title, description }) {
   return (
-    <div className="block md:flex items-center md:mt-28 mt-36">
+    <div className="block md:flex items-center md:mt-32 space-x-4">
       {/* Image Container */}
-      <div className="md:w-1/2 justify-center items-center m-3">
-        <img src={img} alt="" className="w-full h-full m-auto md:w-2/4" />
+      <div className="md:w-1/2 w-full flex justify-center items-center p-4">
+        <img src={imgSrc} alt="" className="w-full h-full md:w-[540px] m-auto justify-center items-center text-center mt-48 md:mt-0" />
       </div>
+
       {/* Text Content */}
-      <div className="md:w-1/2 justify-center items-center leading-2 mt-12 md:mt-0">
-        <h1 className="text-3xl font-medium leading-tight text-secondary ml-4">
-          Light, Fast & Powerful
+      <div className="md:w-1/2 w-full p-4">
+        <h1 className="text-3xl font-medium leading-tight text-secondary">
+          {title}
         </h1>
-        <p className="mt-4 text-lg text-[#6F7CB2] ml-4">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
-          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
-          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
-          consequat massa quis enim.
+        <p className="mt-4 text-lg text-[#6F7CB2] text-justify">
+          {description}
         </p>
       </div>
     </div>
   );
 }
 
-export default SectionThree;
+export default Section;
