189d6a4dceee4ad00cc96da494c25aa5ebac2d12
Nirma Abro <NirmaAbro143@gmail.com>
2024-08-17 21:44:36 -0600

    footer bug fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -65,3 +65,6 @@ function Footer() {
 }
 
 export default Footer;
+
+
+
--- src/components/SectionThree.jsx
+++ src/components/SectionThree.jsx
@@ -1,14 +1,43 @@
+// import React from "react";
+
+// function Section({ imgSrc, title, description }) {
+//   return (
+//     <div className="block md:flex items-center md:mt-32 space-x-4">
+//       {/* Image Container */}
+//       <div className="md:w-1/2 w-full flex justify-center items-center p-4">
+//         <img
+//           src={imgSrc}
+//           alt=""
+//           className="w-full h-full md:w-[480px] m-auto justify-center items-center text-center mt-48 md:mt-0"
+//         />
+//       </div>
+
+//       {/* Text Content */}
+//       <div className="md:w-1/2 w-full p-4">
+//         <h1 className="text-3xl font-medium leading-tight text-secondary">
+//           {title}
+//         </h1>
+//         <p className="mt-4 text-lg text-[#6F7CB2] text-justify">
+//           {description}
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Section;
+
 import React from "react";
 
 function Section({ imgSrc, title, description }) {
   return (
-    <div className="block md:flex items-center md:mt-32 space-x-4">
+    <div className="block md:flex items-center md:mt-32 mt-24">
       {/* Image Container */}
-      <div className="md:w-1/2 w-full flex justify-center items-center p-4">
+      <div className="md:w-1/2 w-full flex justify-center items-center p-0 md:p-4">
         <img
           src={imgSrc}
           alt=""
-          className="w-full h-full md:w-[480px] m-auto justify-center items-center text-center mt-48 md:mt-0"
+          className="w-2/3 h-auto md:w-[480px] m-auto"
         />
       </div>
 
